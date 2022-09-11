1cb1ab370f2a6379bec9d8a973e81618b3374df0
walid <walid.alouini1@gmail.com>
2022-09-11 18:00:45 +0200

    fix(Calendar): fix display event in calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import useWindowSize from '../hooks/windowSize/useWindowSize'
 import "react-big-calendar/lib/css/react-big-calendar.css";
-// import events from "../events";
-
+import input from "../input.json";
+import { parsingStringToDate, getDateAfterDuration } from '../utils/Utils';
 
 const CalendarGrid = () => {
     const { height, width } = useWindowSize()
@@ -13,22 +13,44 @@ const CalendarGrid = () => {
 
     const localizer = momentLocalizer(moment);
 
+    const [events, setEvents] = useState([])
+
+    const handelEvent = (currentEvent) => {
+      setEvents((prevEvents) => [
+        ...prevEvents,
+        {
+          id: currentEvent.id,
+          title: `title: ${currentEvent.id}`,
+          start: parsingStringToDate(currentEvent.start),
+          end: getDateAfterDuration(currentEvent.start, currentEvent.duration),
+        },
+      ]);
+    }
+
+    useEffect(()=>{
+      input.forEach(inputEvent => {
+        handelEvent(inputEvent)
+      })
+    },[])
+
     return(
-        <Calendar
-            localizer={localizer}
-            style={{ height: height, width: width }}
+      <Calendar
+        style={{ height: height, width: width }}
+        toolbar={false}
+        events={events}
+        defaultView="day"
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        dayLayoutAlgorithm="no-overlap"
 
-            toolbar={false}
-            defaultDate={new Date(today.getFullYear(), today.getMonth()+1, today.getDate())}
-            defaultView="day"
-            // events={events}
-            views={["day"]}
+        defaultDate={new Date(today.getFullYear(), today.getMonth()+1, today.getDate())}
 
-            // start time 9:00am
-            min={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 9)}
-            // end time 9:00pm
-            max={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 21)}
-        />
-    )
+        // start time 9:00am
+        min={new Date(today.getFullYear(), today.getMonth()+1, today.getDate(), 9)}
+        // end time 9:00pm
+        max={new Date(today.getFullYear(), today.getMonth()+1, today.getDate(), 21)}
+      />
+  )
 }
 export default CalendarGrid
