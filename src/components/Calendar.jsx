6cadcf4daf264153e3326e1193423fb5772cfeea
walid <walid.alouini1@gmail.com>
2022-09-11 18:39:04 +0200

    fix(Calendar): fix display event in calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,37 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import useWindowSize from '../hooks/windowSize/useWindowSize'
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import input from "../input.json";
-import { parsingStringToDate, getDateAfterDuration } from '../utils/Utils';
 
-const CalendarGrid = () => {
+const CalendarGrid = ({events}) => {
     const { height, width } = useWindowSize()
 
     const today = new Date();
 
     const localizer = momentLocalizer(moment);
-
-    const [events, setEvents] = useState([])
-
-    const handelEvent = (currentEvent) => {
-      setEvents((prevEvents) => [
-        ...prevEvents,
-        {
-          id: currentEvent.id,
-          title: `title: ${currentEvent.id}`,
-          start: parsingStringToDate(currentEvent.start),
-          end: getDateAfterDuration(currentEvent.start, currentEvent.duration),
-        },
-      ]);
-    }
-
-    useEffect(()=>{
-      input.forEach(inputEvent => {
-        handelEvent(inputEvent)
-      })
-    },[])
 
     return(
       <Calendar
